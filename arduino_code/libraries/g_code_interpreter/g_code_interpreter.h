794eba481da104b4942349a8612f07414ee6d321
Harry Wei <harry98530@gmail.com>
2020-08-24 00:52:43 -0400

    --Arduino gcode motor speed module, and updates, labType change problem

--- arduino_code/libraries/g_code_interpreter/g_code_interpreter.h
+++ arduino_code/libraries/g_code_interpreter/g_code_interpreter.h
@@ -24,12 +24,12 @@ public:
 /*
 Serial command protocol
 H0 - Handshake
-R - Request data dependent on lab type
+R0 - Request data dependent on lab type
 S0,P#,I#,D# - Set PID gains on arduino
 S1,Z# - Set the setpoint of the controller
 S2,Y# - Lab type 0-angle, 1-ang_velocity
 S3,M# - Turn controller on/off
-S4,T - Set sample time
+S4,T# - Set sample time
 S5,L#,U# - Set lower and upper controller output limits
 
 */
