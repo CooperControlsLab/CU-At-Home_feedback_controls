27e137d8d8b2704ad8f40ad7cb0464a7bbc314b1
Harry Wei <harry98530@gmail.com>
2020-09-16 17:38:37 -0400

    Arduino OL characterization using micros instead of millis to reduce noise, post processing now plots correct columns of csv

--- python_code/python_GUI/post_processing.py
+++ python_code/python_GUI/post_processing.py
@@ -1,7 +1,7 @@
 import csv
 import os
 dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, 'oltest-8V.csv')
+filename = os.path.join(dirname, 'OL_Uno_test_10V_micros.csv')
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
@@ -13,20 +13,28 @@ if __name__ == "__main__":
     with open(filename) as csvDataFile:
         csvReader = csv.reader(csvDataFile)
         for row in csvReader:
-            time.append(row[0])
-            pos.append(row[1])
+            time.append(row[1])
+            pos.append(row[3])
 
 
 timepos = np.column_stack((time[1:],pos[1:])).astype(np.float)
-print(timepos[1:,:])
+# print(timepos[1:,:])
 vel = np.gradient(timepos[:,1], 0.003, axis=0)
 # plt.plot(timepos[:,0], timepos[:,1])
+
+# To find tau
+# steady_state = np.mean(vel[-50:])
+
+# find 0.63 steady_state in vel
+# idx = np.where(np.logical_and(np.greater_equal(np.array(pos),0.63*steady_state), np.greater_equal(np.array(pos)<=0.634*steady_state)))
+# print("Tau = ",time[idx])
 
 lpf = signal.butter(1, 0.1, btype='lowpass', analog=False, output='sos', fs=1)
 filtered = signal.sosfilt(lpf, vel)
 vel_pre_filtered = np.gradient(filtered, timepos[:,0], axis=0)
-# plt.plot(vel_pre_filtered, color='green',  linewidth=1)#[:,0], vel[:,1])
-# plt.plot(timepos[:,0], vel, color='red', linewidth=1)
-# plt.plot(timepos[:,0], filtered, color = 'blue', linewidth=1)
+plt.plot(vel_pre_filtered, color='green',  linewidth=1)#[:,0], vel[:,1])
+plt.plot(timepos[:,0], vel, color='red', linewidth=1)
+plt.plot(timepos[:,0], filtered, color = 'blue', linewidth=1)
 plt.plot(timepos[:,0], np.mod(timepos[:,1], 360), color='blue')
+# plt.vlines(time[idx],color='#fa8072')
 plt.show()
